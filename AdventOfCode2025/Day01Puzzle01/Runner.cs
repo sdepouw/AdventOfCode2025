ecd9dc5d769d890994d3da080d9deaa8066fb55d
Scott DePouw <scott.depouw@gmail.com>
2025-12-05 09:04:10 -0500

    First puzzle

---
+++ AdventOfCode2025/Day01Puzzle01/Runner.cs
@@ -0,0 +1,22 @@
+﻿using Xunit.Abstractions;
+
+namespace AdventOfCode2025.Day01Puzzle01;
+
+public class Runner(ITestOutputHelper testOutputHelper)
+{
+  [Fact]
+  public async Task CalculateTimesHitting0()
+  {
+    SafeDial dial = new(50, 99);
+
+    string[] lines = await File.ReadAllLinesAsync("Day01Puzzle01\\input.txt");
+    foreach (string line in lines)
+    {
+      int ticks = int.Parse(line[1..]);
+      if (line.StartsWith("L")) dial.MoveLeft(ticks);
+      if (line.StartsWith("R")) dial.MoveRight(ticks);
+    }
+    
+    testOutputHelper.WriteLine(dial.TimesHitting0.ToString());
+  }
+}
---
+++ AdventOfCode2025/Day01Puzzle01/SafeDial.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode2025.Day01Puzzle01;
+
+public class SafeDial(int startingPosition, int maxPosition)
+{
+  private int _position = startingPosition;
+  public int TimesHitting0;
+  
+  public void MoveLeft(int ticks)
+  {
+    for (int i = 0; i < ticks; i++)
+    {
+      _position--;
+      if (_position < 0) _position = maxPosition;
+    }
+    if (_position == 0) TimesHitting0++;
+  }
+
+  public void MoveRight(int ticks)
+  {
+    for (int i = 0; i < ticks; i++)
+    {
+      _position++;
+      if (_position > maxPosition) _position = 0;
+    }
+    if (_position == 0) TimesHitting0++;
+  }
+}
